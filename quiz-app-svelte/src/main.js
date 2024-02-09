0a4fb5249f665e4f241c2ef60bb6e60ed88b39e1
Enzo <enzo.erdmann@gmail.com>
2024-02-09 09:40:34 +0100

    couleur

--- quiz-app-svelte/src/main.js
+++ quiz-app-svelte/src/main.js
@@ -4,4 +4,4 @@ const app = new App({
   target: document.body,
 });
 
-document.body.style = "  background: linear-gradient(to bottom, #3e7ca8, #d3d3d3); ";
+document.body.style = "  background: linear-gradient(to bottom, #3e7ca8, #3e7caa); ";
